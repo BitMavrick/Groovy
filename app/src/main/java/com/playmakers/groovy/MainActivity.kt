feb41e68e4366e21f43cb1d134daf7c8697ed289
Mehedi Hasan <email.mehedi.bd@gmail.com>
2023-07-30 20:33:25 +0600

    Permissions in progress

--- app/src/main/java/com/playmakers/groovy/MainActivity.kt
+++ app/src/main/java/com/playmakers/groovy/MainActivity.kt
@@ -5,29 +5,41 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.annotation.RequiresApi
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.ExperimentalMaterialApi
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.ui.Modifier
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.google.accompanist.pager.ExperimentalPagerApi
-import com.playmakers.groovy.ui.home.HomeScreen
+import com.playmakers.groovy.ui.permissions.PermissionsViewModel
 import com.playmakers.groovy.ui.theme.GroovyTheme
 
 class MainActivity : ComponentActivity() {
+
+    @RequiresApi(Build.VERSION_CODES.TIRAMISU)
+    private val permissionsToRequest = arrayOf(
+        android.Manifest.permission.READ_EXTERNAL_STORAGE,
+        android.Manifest.permission.READ_MEDIA_AUDIO,
+    )
+
     @OptIn(ExperimentalMaterialApi::class, ExperimentalPagerApi::class)
     @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             GroovyTheme {
+
+                val viewModel = viewModel<PermissionsViewModel>()
+
                 // A surface container using the 'background' color from the theme
+
+                /*
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
                     HomeScreen()
                 }
+                */
+
+
             }
         }
     }
--- app/src/main/java/com/playmakers/groovy/ui/permissions/PermissionsViewModel.kt
+++ app/src/main/java/com/playmakers/groovy/ui/permissions/PermissionsViewModel.kt
@@ -1,4 +1,22 @@
 package com.playmakers.groovy.ui.permissions
 
-class PermissionsViewModel {
+import androidx.compose.runtime.mutableStateListOf
+import androidx.lifecycle.ViewModel
+
+class PermissionsViewModel: ViewModel() {
+
+    val visiblePermissionDialogQueue = mutableStateListOf<String>()
+
+    fun dismissDialog() {
+        visiblePermissionDialogQueue.removeFirst()
+    }
+
+    fun onPermissionResult(
+        permission: String,
+        isGranted: Boolean
+    ) {
+        if(!isGranted && !visiblePermissionDialogQueue.contains(permission)) {
+            visiblePermissionDialogQueue.add(permission)
+        }
+    }
 }
