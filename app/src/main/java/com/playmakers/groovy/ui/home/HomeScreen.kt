4c5c8a3cbf23c1d2d2a40a10b19fed36dc77d61a
Mehedi Hasan <email.mehedi.bd@gmail.com>
2023-08-04 20:28:20 +0600

    Done with the music scaling

--- app/src/main/java/com/playmakers/groovy/ui/home/HomeScreen.kt
+++ app/src/main/java/com/playmakers/groovy/ui/home/HomeScreen.kt
@@ -15,6 +15,7 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
@@ -36,6 +37,7 @@ import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.asImageBitmap
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
@@ -176,7 +178,10 @@ private fun Track(music : Music){
 
             if (bitmap != null) {
                 Image(
-                    bitmap = bitmap.asImageBitmap(), contentDescription = null
+                    bitmap = bitmap.asImageBitmap(),
+                    contentDescription = null,
+                    contentScale = ContentScale.Crop,
+                    modifier = Modifier.aspectRatio(1f)
                 )
             }
         }
@@ -201,7 +206,6 @@ private fun Track(music : Music){
                 fontSize = 12.sp
             )
         }
-
     }
 }
 
@@ -254,7 +258,6 @@ fun FavoritesScreen(){
         }
     }
 }
-
 
 @OptIn(ExperimentalMaterialApi::class, ExperimentalPagerApi::class)
 @Preview
