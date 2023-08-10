0b30fc7a5b98eeb3bba3f193f70eb0a4226ef54b
Mehedi Hasan <email.mehedi.bd@gmail.com>
2023-08-10 18:24:27 +0600

    Change the bottom sheet

--- app/src/main/java/com/playmakers/groovy/ui/screens/homeScreen/Screen.kt
+++ app/src/main/java/com/playmakers/groovy/ui/screens/homeScreen/Screen.kt
@@ -1,9 +1,16 @@
     package com.playmakers.groovy.ui.screens.homeScreen
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.material3.BottomSheetScaffold
+import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.ListItem
@@ -11,39 +18,53 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.material3.rememberBottomSheetScaffoldState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.playmakers.groovy.ui.composables.HomeContent
+import kotlinx.coroutines.launch
 
     @Preview(showBackground = true)
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun HomeScreen() {
-    val scaffoldState = rememberBottomSheetScaffoldState()
-    val scrollBehavior = TopAppBarDefaults.pinnedScrollBehavior()
+        val scope = rememberCoroutineScope()
+        val scaffoldState = rememberBottomSheetScaffoldState()
 
     BottomSheetScaffold(
-        modifier = Modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
+        scaffoldState = scaffoldState,
+        sheetPeekHeight = 128.dp, // Default is 128.dp (0 dp means it hidden)
 
         sheetContent = {
-            LazyColumn {
-                items(50) {
-                    ListItem(
-                        headlineContent = { Text("Item $it") },
-                        leadingContent = {
-                            Icon(
-                                Icons.Default.Favorite,
-                                contentDescription = "Localized description"
-                            )
-                        }
-                    )
+            Box(
+                Modifier
+                    .fillMaxWidth()
+                    .height(128.dp),
+                contentAlignment = Alignment.Center
+            ) {
+                Text("Swipe up")
+            }
+            Column(
+                Modifier
+                    .fillMaxWidth()
+                    .padding(64.dp),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Text("Sheet content")
+                Spacer(Modifier.height(100.dp))
+                Button(
+                    onClick = {
+                        scope.launch { scaffoldState.bottomSheetState.partialExpand() }
+                    }
+                ) {
+                    Text("Click to collapse sheet")
                 }
             }
         },
-        scaffoldState = scaffoldState,
-        sheetPeekHeight = 128.dp, // Default is 128.dp (0 dp means it hidden)
+
     ) {
         HomeContent()
     }
