cecc4dcc8b666c1d11b86dee6ac4cf7057923f57
Mehedi Hasan <email.mehedi.bd@gmail.com>
2023-08-10 10:34:38 +0600

    Done with the permission screen design

--- app/src/main/java/com/playmakers/groovy/ui/util/PermissionsHandler.kt
+++ app/src/main/java/com/playmakers/groovy/ui/util/PermissionsHandler.kt
@@ -12,9 +12,12 @@ import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -68,18 +71,30 @@ fun PermissionHandler(
         HomeScreen()
     }else{
         Column(
-            modifier = Modifier.fillMaxSize(),
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(16.dp),
             verticalArrangement = Arrangement.Center,
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
-            Button(
+            Text(
+                text = "Groovy, \nA Simple, Minimalistic \n& Open-source Music Player",
+                style = MaterialTheme.typography.displayLarge
+            )
+
+            Spacer(modifier = Modifier.height(72.dp))
+
+            OutlinedButton(
                 onClick = {
                     audioPermissionResultLauncher.launch(
                         musicPermission()
                     )
                 }
             ) {
-                Text(text = "Please allow the audio permission")
+                Text(
+                    text = "Allow audio permission",
+                    style = MaterialTheme.typography.titleMedium
+                )
             }
         }
     }
@@ -135,14 +150,26 @@ fun openAppSettings(activity: Activity) {
 @Preview(showBackground = true)
 private fun PermissionHandlerPreview() {
     Column(
-        modifier = Modifier.fillMaxSize().padding(16.dp),
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp),
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
+        Text(
+            text = "Groovy, \nA Simple, Minimalistic \n& Open-source Music Player",
+            style = MaterialTheme.typography.displayLarge
+        )
+
+        Spacer(modifier = Modifier.height(72.dp))
+
         OutlinedButton(
             onClick = {}
         ) {
-            Text(text = "Allow audio permission")
+            Text(
+                text = "Allow audio permission",
+                style = MaterialTheme.typography.titleMedium
+            )
         }
     }
 }
