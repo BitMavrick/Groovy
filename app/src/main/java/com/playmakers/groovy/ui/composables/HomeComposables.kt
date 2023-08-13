f2917ab984efc5b543e8181d65c1e0aecf83ed14
Mehedi Hasan <email.mehedi.bd@gmail.com>
2023-08-13 13:08:19 +0600

    Progressing ...

--- app/src/main/java/com/playmakers/groovy/ui/composables/HomeComposables.kt
+++ app/src/main/java/com/playmakers/groovy/ui/composables/HomeComposables.kt
@@ -1,16 +1,11 @@
 package com.playmakers.groovy.ui.composables
 
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Menu
-import androidx.compose.material.icons.filled.MoreVert
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.material.icons.filled.Star
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -23,11 +18,9 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.semantics.isTraversalGroup
 import androidx.compose.ui.semantics.semantics
-import androidx.compose.ui.semantics.traversalIndex
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 
--- app/src/main/java/com/playmakers/groovy/ui/screens/homeScreen/Screen.kt
+++ app/src/main/java/com/playmakers/groovy/ui/screens/homeScreen/Screen.kt
@@ -1,23 +1,27 @@
 package com.playmakers.groovy.ui.screens.homeScreen
 
 import androidx.compose.foundation.layout.ExperimentalLayoutApi
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.consumeWindowInsets
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material.icons.filled.Shuffle
 import androidx.compose.material3.ExtendedFloatingActionButton
 import androidx.compose.material3.FabPosition
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.playmakers.groovy.ui.composables.TopSearchBar
 
-@OptIn(ExperimentalMaterial3Api::class, ExperimentalLayoutApi::class)
+@OptIn(ExperimentalLayoutApi::class)
 @Preview
 @Composable
 fun HomeScreen() {
@@ -29,7 +33,12 @@ fun HomeScreen() {
             ExtendedFloatingActionButton(
                 onClick = { /* fab click handler */ }
             ) {
-                Text("Inc")
+                Icon(
+                    imageVector = Icons.Default.Shuffle,
+                    contentDescription = null
+                )
+                Spacer(modifier = Modifier.width(4.dp))
+                Text("Shuffle")
             }
         },
         content = { innerPadding ->
@@ -37,9 +46,15 @@ fun HomeScreen() {
                 modifier = Modifier.consumeWindowInsets(innerPadding),
                 contentPadding = innerPadding
             ) {
+                item {
+                    Text("This is some text at the top of the list.")
+                }
                 val list = List(100) { "Text $it" }
                 items(count = list.size) {
-                    Text(list[it], Modifier.fillMaxWidth().padding(horizontal = 16.dp))
+                    Text(list[it],
+                        Modifier
+                            .fillMaxWidth()
+                            .padding(horizontal = 16.dp))
                 }
             }
         }
