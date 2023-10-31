47da48060a3e11d7c2e4d70c8848eb69d164beca
Mehedi Hasan <email.mehedi.bd@gmail.com>
2023-10-31 18:24:39 +0600

    Successfully displaying the music list

--- app/src/main/java/com/playmakers/groovy/ui/GroovyApp.kt
+++ app/src/main/java/com/playmakers/groovy/ui/GroovyApp.kt
@@ -1,7 +1,12 @@
 package com.playmakers.groovy.ui
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.playmakers.groovy.GroovyViewModelProvider
 import com.playmakers.groovy.stateModel.MainViewModel
@@ -11,6 +16,16 @@ fun GroovyApp() {
     val mainViewModel: MainViewModel = viewModel(
         factory = GroovyViewModelProvider.Factory
     )
+
+    LazyColumn(
+        Modifier.fillMaxSize()
+    ){
+        item{
+            for(music in mainViewModel.getMusic()){
+                Text(text = music.title)
+            }
+        }
+    }
     
-    Text(text = "Hello this is Groovy!")
+
 }
