1f6eafe5d9df4dd11b7349aeb230c3a694e90a7b
Mehedi Hasan <email.mehedi.bd@gmail.com>
2023-11-01 19:08:00 +0600

    Done for today

--- app/src/main/java/com/playmakers/groovy/model/Music.kt
+++ app/src/main/java/com/playmakers/groovy/model/Music.kt
@@ -1,9 +1,10 @@
 package com.playmakers.groovy.model
 
 import android.net.Uri
+import androidx.media3.common.MediaItem
 
 data class Music (
-    val id: Long,
+    val id: String,
     val title: String,
     val artist: String,
     val album: String,
@@ -11,3 +12,15 @@ data class Music (
     val path: String,
     val contentUri: Uri?,
 )
+
+// TODO: >>>>>>>>>> The toMusic function have some serious integration bug, we have to fix this <<<<<<<<<<
+fun MediaItem.toMusic() =
+    Music(
+        id = mediaId,
+        title = mediaMetadata.title.toString(),
+        artist = mediaMetadata.artist.toString(),
+        album = mediaMetadata.albumTitle.toString(),
+        duration = 0L,
+        path = "No Path",
+        contentUri = null
+    )
--- app/src/main/java/com/playmakers/groovy/service/MusicPlayBackControl.kt
+++ app/src/main/java/com/playmakers/groovy/service/MusicPlayBackControl.kt
@@ -1,57 +1,131 @@
 package com.playmakers.groovy.service
 
+import android.content.ComponentName
 import android.content.Context
+import android.net.Uri
+import androidx.media3.common.MediaItem
+import androidx.media3.common.MediaMetadata
+import androidx.media3.common.Player
+import androidx.media3.session.MediaController
+import androidx.media3.session.SessionToken
+import com.google.common.util.concurrent.ListenableFuture
+import com.google.common.util.concurrent.MoreExecutors
 import com.playmakers.groovy.model.Music
 import com.playmakers.groovy.model.PlayBackControl
 import com.playmakers.groovy.model.PlayerState
+import com.playmakers.groovy.model.toMusic
 
 class MusicPlayBackControl(context: Context) : PlayBackControl {
-    override var mediaControllerCallback: ((playerState: PlayerState, currentMusic: Music?, currentPosition: Long, totalDuration: Long, isShuffleEnabled: Boolean, isRepeatOneEnabled: Boolean) -> Unit)?
-        get() = TODO("Not yet implemented")
-        set(value) {}
+
+    private var mediaControllerFuture: ListenableFuture<MediaController>
+
+    private val mediaController: MediaController?
+        get() = if (mediaControllerFuture.isDone) mediaControllerFuture.get() else null
+
+    override var mediaControllerCallback: (
+        (playerState: PlayerState,
+         currentMusic: Music?,
+         currentPosition: Long,
+         totalDuration: Long,
+         isShuffleEnabled: Boolean,
+         isRepeatOneEnabled: Boolean) -> Unit
+    )? = null
+
+    init {
+        val sessionToken =
+            SessionToken(context, ComponentName(context, MusicPlayBackService::class.java))
+        mediaControllerFuture = MediaController.Builder(context, sessionToken).buildAsync()
+        mediaControllerFuture.addListener({ controllerListener() }, MoreExecutors.directExecutor())
+    }
+
+    private fun controllerListener() {
+        mediaController?.addListener(object : Player.Listener {
+            override fun onEvents(player: Player, events: Player.Events) {
+                super.onEvents(player, events)
+
+                with(player) {
+                    mediaControllerCallback?.invoke(
+                        playbackState.toPlayerState(isPlaying),
+                        currentMediaItem?.toMusic(),
+                        currentPosition.coerceAtLeast(0L),
+                        duration.coerceAtLeast(0L),
+                        shuffleModeEnabled,
+                        repeatMode == Player.REPEAT_MODE_ONE
+                    )
+                }
+            }
+        })
+    }
+
+    private fun Int.toPlayerState(isPlaying: Boolean) =
+        when (this) {
+            Player.STATE_IDLE -> PlayerState.STOPPED
+            Player.STATE_ENDED -> PlayerState.STOPPED
+            else -> if (isPlaying) PlayerState.PLAYING else PlayerState.PAUSED
+        }
 
     override fun addMediaItems(musics: List<Music>) {
-        TODO("Not yet implemented")
+        val mediaItems = musics.map {
+            MediaItem.Builder()
+                .setMediaId(it.contentUri.toString())
+                .setUri(it.contentUri.toString())
+                .setMediaMetadata(
+                    MediaMetadata.Builder()
+                        .setTitle(it.title)
+                        .setArtist(it.artist)
+                        .setArtworkUri(Uri.parse(it.contentUri.toString()))
+                        .build()
+                )
+                .build()
+        }
+
+        mediaController?.setMediaItems(mediaItems)
     }
 
     override fun play(mediaItemIndex: Int) {
-        TODO("Not yet implemented")
+        mediaController?.apply {
+            seekToDefaultPosition(mediaItemIndex)
+            playWhenReady = true
+            prepare()
+        }
     }
 
     override fun resume() {
-        TODO("Not yet implemented")
+        mediaController?.play()
     }
 
     override fun pause() {
-        TODO("Not yet implemented")
+        mediaController?.pause()
     }
 
     override fun seekTo(position: Long) {
-        TODO("Not yet implemented")
+        mediaController?.seekTo(position)
     }
 
     override fun skipNext() {
-        TODO("Not yet implemented")
+        mediaController?.seekToNext()
     }
 
     override fun skipPrevious() {
-        TODO("Not yet implemented")
+        mediaController?.seekToPrevious()
     }
 
     override fun setShuffleModeEnabled(isEnabled: Boolean) {
-        TODO("Not yet implemented")
+        mediaController?.shuffleModeEnabled = isEnabled
     }
 
     override fun setRepeatOneEnabled(isEnabled: Boolean) {
-        TODO("Not yet implemented")
+        mediaController?.repeatMode = if (isEnabled) {
+            Player.REPEAT_MODE_ONE
+        } else {
+            Player.REPEAT_MODE_OFF
+        }
     }
 
-    override fun getCurrentPosition(): Long {
-        TODO("Not yet implemented")
-    }
+    override fun getCurrentPosition() = mediaController?.currentPosition ?: 0L
 
     override fun destroy() {
-        TODO("Not yet implemented")
+        MediaController.releaseFuture(mediaControllerFuture)
+        mediaControllerCallback = null
     }
-
 }
