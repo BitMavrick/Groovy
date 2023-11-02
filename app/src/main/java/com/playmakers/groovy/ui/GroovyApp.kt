eebf19fc09c178b2fe769c7ecf3d80fb2d40121c
Mehedi Hasan <email.mehedi.bd@gmail.com>
2023-11-02 22:38:41 +0600

    Need Bug fixing ...

--- app/src/main/java/com/playmakers/groovy/ui/GroovyApp.kt
+++ app/src/main/java/com/playmakers/groovy/ui/GroovyApp.kt
@@ -1,30 +1,52 @@
 package com.playmakers.groovy.ui
 
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.Card
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.playmakers.groovy.GroovyViewModelProvider
+import com.playmakers.groovy.stateModel.MainEvent
 import com.playmakers.groovy.stateModel.MainViewModel
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun GroovyApp() {
     val mainViewModel: MainViewModel = viewModel(
         factory = GroovyViewModelProvider.Factory
     )
 
+    val event = mainViewModel::onEvent
+
     LazyColumn(
         Modifier.fillMaxSize()
     ){
         item{
-            Text(text = "Hello This is Groovy Music")
+            Text(
+                text = "Groovy Music",
+                modifier = Modifier.padding(vertical = 6.dp)
+            )
         }
 
         item{
             for(music in mainViewModel.getMusic()){
-                Text(text = music.title)
+                Card(
+                    onClick = {
+                        event(MainEvent.OnMusicSelected(music))
+                        event(MainEvent.PlayMusic)
+                    }
+                ) {
+                    Text(
+                        text = music.title,
+                        modifier = Modifier.padding(vertical = 6.dp),
+                    )
+                }
             }
         }
     }
