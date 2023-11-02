42ec57a2b11fd0ce597ef6a05c21188fd75d0a68
Mehedi Hasan <email.mehedi.bd@gmail.com>
2023-11-02 19:45:22 +0600

    Saving so far ...

--- app/src/main/java/com/playmakers/groovy/GroovyApplication.kt
+++ app/src/main/java/com/playmakers/groovy/GroovyApplication.kt
@@ -2,12 +2,16 @@ package com.playmakers.groovy
 
 import android.app.Application
 import com.playmakers.groovy.model.Music
+import com.playmakers.groovy.model.PlayBackControl
+import com.playmakers.groovy.service.MusicPlayBackControl
 import com.playmakers.groovy.service.getMusic
 
 class GroovyApplication : Application() {
     lateinit var musicList: List<Music>
+    lateinit var playBackControl: PlayBackControl
     override fun onCreate() {
         super.onCreate()
         musicList = getMusic(this)
+        playBackControl = MusicPlayBackControl(this)
     }
 }
--- app/src/main/java/com/playmakers/groovy/stateModel/MainViewModel.kt
+++ app/src/main/java/com/playmakers/groovy/stateModel/MainViewModel.kt
@@ -1,10 +1,35 @@
 package com.playmakers.groovy.stateModel
 
 import androidx.lifecycle.ViewModel
+import com.playmakers.groovy.control.PlayMusic
 import com.playmakers.groovy.model.Music
 
-class MainViewModel(private val musicList: List<Music>) : ViewModel(){
+class MainViewModel(
+    private val musicList: List<Music>,
+    private val playMusic: PlayMusic
+) : ViewModel() {
+
     fun getMusic() : List<Music> {
         return musicList
+    }
+
+    fun onEvent(event: MainEvent){
+//        when (event) {
+//            MainEvent.PlayMusic -> playMusic()
+//
+//
+//        }
+    }
+
+    private fun playMusic() {
+
+    }
+
+    private fun resumeMusic() {
+        //resumeMusicUseCase()
+    }
+
+    private fun pauseMusic() {
+        //pauseMusicUseCase()
     }
 }
