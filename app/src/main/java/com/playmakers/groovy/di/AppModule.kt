a8f91ac8895470d4b5b9f779a4a115224c8ae4ef
Mehedi Hasan <email.mehedi.bd@gmail.com>
2023-11-16 22:34:15 +0600

    Done for today

--- app/src/main/java/com/playmakers/groovy/di/AppModule.kt
+++ app/src/main/java/com/playmakers/groovy/di/AppModule.kt
@@ -12,6 +12,7 @@ import javax.inject.Singleton
 @Module
 @InstallIn(SingletonComponent::class)
 object AppModule {
+
     @Provides
     @Singleton
     fun provideMusicRepository(app: Application) : MusicRepository{
