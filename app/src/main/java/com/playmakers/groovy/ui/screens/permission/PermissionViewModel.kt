5fec334444f21c3fafa15c2ba94fc9098e1b942f
Mehedi Hasan <email.mehedi.bd@gmail.com>
2023-11-17 20:53:22 +0600

    Setting up the permission screen and alert dialog

--- app/src/main/java/com/playmakers/groovy/ui/screens/permission/PermissionViewModel.kt
+++ app/src/main/java/com/playmakers/groovy/ui/screens/permission/PermissionViewModel.kt
@@ -4,7 +4,7 @@ import androidx.compose.runtime.mutableStateListOf
 import androidx.lifecycle.ViewModel
 
 class PermissionViewModel : ViewModel() {
-    private val visiblePermissionDialogQueue = mutableStateListOf<String>()
+    val visiblePermissionDialogQueue = mutableStateListOf<String>()
     fun dismissDialog() {
         visiblePermissionDialogQueue.removeFirst()
     }
