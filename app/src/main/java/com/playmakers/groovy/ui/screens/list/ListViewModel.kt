6f6111884fde3db92fb6bc4a0606064dc6166ffd
Mehedi Hasan <email.mehedi.bd@gmail.com>
2023-11-19 11:52:06 +0600

    Optimizing the code

--- app/src/main/java/com/playmakers/groovy/ui/screens/list/ListViewModel.kt
+++ app/src/main/java/com/playmakers/groovy/ui/screens/list/ListViewModel.kt
@@ -2,13 +2,11 @@ package com.playmakers.groovy.ui.screens.list
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.playmakers.groovy.domain.model.Music
 import com.playmakers.groovy.domain.repository.MusicRepository
 import com.playmakers.groovy.ui.util.ListState
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 import javax.inject.Inject
