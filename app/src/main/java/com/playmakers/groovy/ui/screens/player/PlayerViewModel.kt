9ce1423dc48ec5697920a1270bfad04429708945
Mehedi Hasan <email.mehedi.bd@gmail.com>
2023-12-05 22:00:25 +0600

    Saving ..

--- app/src/main/java/com/playmakers/groovy/ui/screens/player/PlayerViewModel.kt
+++ app/src/main/java/com/playmakers/groovy/ui/screens/player/PlayerViewModel.kt
@@ -2,12 +2,24 @@ package com.playmakers.groovy.ui.screens.player
 
 import androidx.lifecycle.ViewModel
 import com.playmakers.groovy.controller.AddMusic
+import com.playmakers.groovy.controller.DestroyMusicPlaybackControl
+import com.playmakers.groovy.controller.GetMusicPosition
+import com.playmakers.groovy.controller.PauseMusic
+import com.playmakers.groovy.controller.PlayMusic
+import com.playmakers.groovy.controller.ResumeMusic
+import com.playmakers.groovy.controller.SetMediaControlCallback
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
 @HiltViewModel
 class PlayerViewModel @Inject constructor (
-    private val addMusic: AddMusic
+    private val addMusic: AddMusic,
+    private val playMusic: PlayMusic,
+    private val resumeMusic: ResumeMusic,
+    private val pauseMusic: PauseMusic,
+    private val setMediaControlCallBack:SetMediaControlCallback,
+    private val getCurrentMediaPosition: GetMusicPosition,
+    private val destroyController: DestroyMusicPlaybackControl
 ) : ViewModel() {
 
 }
